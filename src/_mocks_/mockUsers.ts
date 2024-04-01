d85cb2f4194f5cf53d2421d8c01d10f0e231b644
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-04-01 22:05:08 +0300

    Создан пример запроса с параметром, обнуляемым возвращаемым значением и переименованием запроса в схеме GraphQL.

---
+++ src/_mocks_/mockUsers.ts
@@ -0,0 +1,17 @@
+export const mockUsers = [
+  {
+    id: 1,
+    username: 'admin',
+    displayName: 'Admin',
+  },
+  {
+    id: 2,
+    username: 'Олег',
+    displayName: 'Oleg',
+  },
+  {
+    id: 3,
+    username: 'Андрей',
+    displayName: 'Andrey',
+  },
+];
--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,14 +1,12 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { User } from '../models/User';
+import { mockUsers } from '../../_mocks_/mockUsers';
 
 @Resolver()
 export class UserResolver {
-  @Query(() => User)
-  getUser() {
-    return {
-      id: 1,
-      username: 'admin',
-      displayName: 'Admin',
-    };
+  //name: 'userById' is the name of the query в схеме GraphQL
+  @Query(() => User, { nullable: true, name: 'userById' })
+  getUserById(@Args('id', { type: () => Int }) id: number): User {
+    return mockUsers.find((user) => user.id === id);
   }
 }
