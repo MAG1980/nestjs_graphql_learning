a87afc976a482ae39872d0ca1781e86a47788859
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-04-05 10:49:58 +0300

    Выполнен рефакторинг DataSource.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
 import * as path from 'path';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { getDatasourceOptions } from './config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import * as process from 'process';
-import { UserModule } from './user/user.module';
 import { UserSettingsModule } from './user-settings/user-settings.module';
+import { datasourceOptions } from './dataSource';
+import { UserModule } from './user/user.module';
 
 const { join } = path;
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({ ...getDatasourceOptions() }),
+    TypeOrmModule.forRoot(datasourceOptions),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
--- src/dataSource/index.ts
+++ src/dataSource/index.ts
@@ -1,6 +1,25 @@
-import { DataSource } from 'typeorm';
-import { getDatasourceOptions } from '../config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { URL } from 'url';
+import { config } from 'dotenv';
 
-export const AppDataSource = new DataSource({
-  ...getDatasourceOptions(),
-});
+config();
+
+const dbUrl = new URL(process.env.DATABASE_URL);
+const routingId = dbUrl.searchParams.get('options');
+dbUrl.searchParams.delete('options');
+
+export const datasourceOptions: DataSourceOptions = {
+  type: 'cockroachdb',
+  url: dbUrl.toString(),
+  ssl: true,
+  extra: {
+    options: routingId,
+  },
+  timeTravelQueries: false,
+  entities: [__dirname + '/../graphql/models/*{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/../../migrations/**/*{.ts,.js}'],
+  // logging:true
+};
+
+export default new DataSource(datasourceOptions);
