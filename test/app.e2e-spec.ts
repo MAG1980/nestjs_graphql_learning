c4dfcdf11aef4ad796d4bd6bad82aa05702c5aeb
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-04-08 10:13:12 +0300

    Добавлен скрипт mg-test:run для выполнения миграций в тестовой БД, но необходимость его использования отсутствует, т.к. настроена автоматическая синхронизация БД (очистка данных с повторным созданием структуры) перед каждым запуском всех тестов. Создан тест mutation{createUser}.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,16 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { DataSource } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
+  // beforeEach - выполняется перед каждым тестом
+  // инициализировать приложение перед каждым тестом не требуется,
+  // поэтому меняем beforeEach beforeAll (выполняется перед всеми тестами)
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    // Получение доступа к подключению к базе данных
+    const dataSource = app.get(DataSource);
+
+    //Удаление данных из БД за счёт аргумента true (синхронизация базы данных c очисткой)
+    await dataSource.synchronize(true);
     await app.init();
   });
 
@@ -31,6 +40,23 @@ describe('AppController (e2e)', () => {
           console.log(res);
           expect(res.body.data.getAllUsers.length).toBe(0);
           expect(res.body.data.getAllUsers).toHaveLength(0);
+        });
+    });
+
+    it('should create a user using createUser mutation ', () => {
+      return request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query:
+            'mutation { createUser (createUserData: {username: "test", displayName: "TestName" }){ id username displayName } }',
+        })
+        .expect(200)
+        .expect((response) => {
+          expect(response.body.data.createUser).toEqual({
+            id: 1,
+            username: 'test',
+            displayName: 'TestName',
+          });
         });
     });
   });
